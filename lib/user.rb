d1bd3f5a5363d022ade3a461b3aef11b52f5c209
Alex Chuang <79676142+AlexChzuang@users.noreply.github.com>
2021-04-01 10:28:41 +0800

    my TDD practice
    class User / Bike

--- lib/user.rb
+++ lib/user.rb
@@ -1,2 +1,26 @@
 class User
+  def initialize(minutes)
+    @minutes = minutes
+  end
+
+  def rent(time)
+    if time < 30
+      5
+    elsif time > 30 and time < 240
+      15
+    elsif time > 240 and time < 480
+      5 + 7 * 10 + 2 * 20
+    elsif time > 480
+      5 + 7 * 10 + 8 * 20 + 80
+    end
+  end
+
+  def borrow(time)
+    if time < 240
+      10 * 4
+    else time > 240 and time < 480
+      10 * 8 + 2 * 20
+    end
+  end
+
 end
